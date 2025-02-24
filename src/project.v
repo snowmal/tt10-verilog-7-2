ac4bc428123059c8b4fe552f6c0fe0f1ca6291ad
Malani Snowden <109847401+snowmal@users.noreply.github.com>
2025-02-24 16:39:42 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,50 +17,17 @@ module eight_bit_adder (
 );
 
   // Our Verilog code:
-  // Priority encoder implementations
-module priority_encoder (
-    input  [15:0] In,     // In[15:0] = {A[7:0], B[7:0]}
-    output reg [7:0] C    // output the address (index) of the first high bit
+// Boolean logic function (shift MUX )
+module rotate_left (
+    input  [7:0] A,
+    input  [7:0] B, // only lower 3 bits of B will determine the rotation amount
+    output [7:0] C
 );
-    always @* begin
-        if (In == 16'b0)
-            C = 8'b11110000;  // special case (all bits are zero)
-        else if (In[15])
-            C = 8'b00001111;
-        else if (In[14])
-            C = 8'b00001110;
-        else if (In[13])
-            C = 8'b00001101;
-        else if (In[12])
-            C = 8'b00001100;
-        else if (In[11])
-            C = 8'b00001011;
-        else if (In[10])
-            C = 8'b00001010;
-        else if (In[9])
-            C = 8'b00001001;
-        else if (In[8])
-            C = 8'b00001000;
-        else if (In[7])
-            C = 8'b00000111;
-        else if (In[6])
-            C = 8'b00000110;
-        else if (In[5])
-            C = 8'b00000101;
-        else if (In[4])
-            C = 8'b00000100;
-        else if (In[3])
-            C = 8'b00000011;
-        else if (In[2])
-            C = 8'b00000010;
-        else if (In[1])
-            C = 8'b00000001;
-        else if (In[0])
-            C = 8'b0;
-        else
-            C = 8'b11110000;
-    end
+    wire [2:0] shift;
+    assign shift = B[2:0];
+    assign C = (A << shift) | (A >> (8 - shift)); // rotate left: shift left by 'shift' and OR with the bits shifted out (wrapped-around)
 endmodule
+
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
