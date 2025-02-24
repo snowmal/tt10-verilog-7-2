6ad711c71cda2c069ca870f8b1b49e9fdbe41064
Malani Snowden <109847401+snowmal@users.noreply.github.com>
2025-02-24 16:18:54 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module eight_bit_adder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,6 +15,16 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+  // Our Verilog code:
+  // Combinational logic circuit w/ 2 8-bit inputs, 1 8-bit output: eight_bit_adder
+  module eight_bit_adds (
+        input [7:0] A,
+        input [7:0] B,
+        output [7:0] Y
+   );
+        assign Y = A + B;
+   endmodule
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
